af8bc284f680e29a2d68aefc16e9e89edaec2d12
Malik Singleton <mrsingleton@gmail.com>
2018-05-01 19:42:30 -0400

    updating with working code

--- chartist.js
+++ chartist.js
@@ -1,13 +1,13 @@
-new Chartist.Line('.ct-chart', {
-  labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+var data = {
+  // A labels array that can contain any sort of values
+  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+  // Our series array that contains series objects or in this case series data arrays
   series: [
-    [12, 9, 7, 8, 5],
-    [2, 1, 3.5, 7, 3],
-    [1, 3, 4, 5, 6]
+    [5, 2, 4, 2, 0]
   ]
-}, {
-  fullWidth: true,
-  chartPadding: {
-    right: 40
-  }
-});
+};
+
+// Create a new line chart object where as first parameter we pass in a selector
+// that is resolving to our chart container element. The Second parameter
+// is the actual data object.
+new Chartist.Line('.ct-chart', data);
